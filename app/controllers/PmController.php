fe8c8f0ddcdf3f0008a284413592099a9a09ebdf
liuhenghaha <1185267428@qq.com>
2015-08-03 11:09:25 +0800

    项目经理详情查看页面

--- app/controllers/PmController.php
+++ app/controllers/PmController.php
@@ -11,11 +11,22 @@
 */
 class PmController extends Base
 {
-	public function indexAction()
+    public function initialize()
     {
-
+          $this->view->setTemplateAfter('base2');
     }
-	
+
+    public function indexAction()
+    {
+        /****************这一段可以抽象成一个通用的方法**********************/
+        $manager=$this->session->get('Manager');
+        $this->view->setVar('page_title','北京政法系统人才测评项目管理平台');
+        $this->view->setVar('user_name',$manager->name);
+        $this->view->setVar('user_id',$manager->username);
+        $this->view->setVar('user_role',"项目经理");
+        /*******************************************************************/
+    }
+
 	public function detailAction()
 	{
 		
