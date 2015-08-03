7752b08dc759f4dc5739570f0142cfe1598b03c7
sxf <sun_xfancy@163.com>
2015-08-03 10:24:11 +0800

    修改了config.php.bk
    Signed-off-by: sxf <sun_xfancy@163.com>

--- app/library/ExcelLoader.php
+++ app/library/ExcelLoader.php
@@ -3,7 +3,7 @@
  * @Author: sxf
  * @Date:   2015-08-02 15:33:40
  * @Last Modified by:   sxf
- * @Last Modified time: 2015-08-03 10:04:19
+ * @Last Modified time: 2015-08-03 10:17:51
  */
 
 /**
@@ -17,55 +17,44 @@ class ExcelLoader
         $db->begin(); 
         if (is_readable($filename))
         {
-            $objexcel = PHPExcel_IOFactory::load("./upload/".$filename);
-            $personsheet = $objexcel->getSheet(0);
+        	try {
+	            $objexcel = PHPExcel_IOFactory::load("./upload/".$filename);
+	            $personsheet = $objexcel->getSheet(0);
 
-
+            }
+			catch (PDOException $ex)
+			{
+				$errors['PDOException'] = $ex->getMessage();
+				$db->rollback();
+				$objexcel->disconnectWorksheets();
+				unlink("./upload/".$file);
+				return $errors;
+			}    
         }
     }
 
-    $excel_col = array( 'C' => 'name',     'D' => 'sex' ,     'E' => 'native' ,'F' => 'education'
-    					'G' => 'birthday', 'H' => 'politics', 'I' => 'professional', 
-    					'J' => 'employer', 'K' => 'unit',     'L' => 'duty')
+    $excel_col = array( 'C' => 'name',     'D' => 'sex',      'E' => 'native',       'F' => 'education',
+    					'G' => 'birthday', 'H' => 'politics', 'I' => 'professional', 'J' => 'employer', 
+    					'K' => 'unit',     'L' => 'duty');
 
-    public function readline_examinee($db)
+    public function readline_examinee ($db)
     {
-    	try {
-			$higestrow = $sheet->getHighestRow();
-			$i = 3;
-			while ($i <= $higestrow) {
-				$k = $sheet->getCell("C".$i)->getValue();
-				if (is_null($k) || $k == "") break;
-				
-				$name = (string)$sheet->getCell($this->excel_col[0].$i)->getValue();
-				$password = (string)$sheet->getCell($this->excel_col[1].$i)->getValue();
-				$district = (string)$sheet->getCell($this->excel_col[2].$i)->getValue();
-				$schoolname = (string)$sheet->getCell($this->excel_col[3].$i)->getValue();
-				$realname = (string)$sheet->getCell($this->excel_col[4].$i)->getValue();
-				$phone = (string)$sheet->getCell($this->excel_col[5].$i)->getValue();
-				$email = (string)$sheet->getCell($this->excel_col[6].$i)->getValue();
-				$ID_number = (string)$sheet->getCell($this->excel_col[7].$i)->getValue();
+		$higestrow = $sheet->getHighestRow();
+		$i = 3;
+		while ($i <= $higestrow) {
+			$k = $sheet->getCell("C".$i)->getValue();
+			if (is_null($k) || $k == "") break;
 
-				$manager = new Manager();
-				if ($manager->signup($username, $password, $phone, $email, $realname, $ID_number))
-				{
-					$this->newschool($schoolname,$district,$manager);
-				} else {
-					throw new PDOException();
-				}
-
-				$i++;
+			$examinee = new Examinee();
+			foreach ($excel_col as $key => $value) {
+				$examinee[$value] = (string)$sheet->getCell($key.$i)->getValue();
 			}
+			
+			if (!$examinee->save()) {
+				
+			}
+			$i++;
 		}
-		catch (PDOException $ex)
-		{
-			$errors['PDOException'] = $ex->getMessage();
-			$this->db->rollback();
-			$objexcel->disconnectWorksheets();
-			unlink("./upload/".$file);
-			// throw $ex;
-			return $errors;
-		}    
 	}
            
 }
