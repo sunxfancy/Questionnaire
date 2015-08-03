a54c9ff7ebdbab8d7e47973cd104498742a88a4d
sxf <sun_xfancy@163.com>
2015-08-03 10:11:40 +0800

    建立项目经理页面
    Signed-off-by: sxf <sun_xfancy@163.com>

--- app/controllers/PmController.php
+++ app/controllers/PmController.php
@@ -3,7 +3,7 @@
  * @Author: sxf
  * @Date:   2015-08-01 16:18:46
  * @Last Modified by:   sxf
- * @Last Modified time: 2015-08-01 16:36:43
+ * @Last Modified time: 2015-08-03 10:09:41
  */
 
 /**
@@ -16,6 +16,28 @@ class PmController extends Base
 
     }
 	
-	
+	public function detailAction()
+	{
+		
+	}
 
+	public function examineeAction()
+	{
+		# code...
+	}
+
+	public function interviewerAction()
+	{
+		# code...
+	}
+
+	public function leaderAction()
+	{
+		# code...
+	}
+
+	public function resultAction()
+	{
+		# code...
+	}
 }
