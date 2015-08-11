154504c0e4fdbc44a48517d8e3061f167d2f436b
liuhenghaha <1185267428@qq.com>
2015-08-11 14:13:36 +0800

    。。

--- app/controllers/ExamineeController.php
+++ app/controllers/ExamineeController.php
@@ -101,6 +101,128 @@ class ExamineeController extends Base
 		$this->leftRender("答题");
 	}
 
+    public function getpaperAction(){
+        $examinee = $this->session->get('Examinee');
+        $project_id = $examinee->project_id;
+        $paper_id = $this->request->getPost("paper_id","string");
+        $questions = $this->getQuestions($project_id,$paper_id);
+    }
+
+    public function getQuestions($project_id,$paper_id)
+    {
+        $project = Pmrel::find(array(
+            "project_id = ?1",
+            "bind"=>array(1=>$project_id)
+            ));
+        $modules_id_array = $this->getModules($project);
+        
+        $indexs = Index::find(array(
+            'module_id IN ({module_id:array})',
+            'bind' => array('module_id' => $modules_id_array)
+        ));
+        $indexs_id_array = $this->getIds($indexs);
+
+        $factor_id_array = $this->getFactor($indexs_id_array);
+
+        $question_id_array = $this->getQlist($factor_id_array,$paper_id);
+
+        $exams = $this->getExam($question_id_array);
+
+        return $exams;
+    }
+
+    public function getFactor($indexs){
+        //$this->view->disable();
+        $factor_id = array();
+        for ($i=0; $i <sizeof($indexs) ; $i++) { 
+            $index = Index::findFirst($indexs[$i]);
+            $children = $index->children;
+            $childrentype = $index->children_type;
+            $children = explode(",",$children );
+            $childrentype = explode(",", $childrentype);
+            for ($j=0; $j < sizeof($childrentype); $j++) { 
+                //0代表index，1代表factor
+                if ($childrentype[$j] == "0") {
+                    $index1 = Index::findFirst(intval($children[$j]));
+                    $children1 = $index1->children;
+                    $children1 = explode(",",$children1);
+                    for ($k=0; $k <sizeof($children1) ; $k++) { 
+                        $children1[$k] = intval($children1[$k]);
+                        $factor_id[] = $children1[$k];
+                    }
+                }
+                else{   
+                        $children[$j] = intval($children[$j]);
+                        $factor_id[] = $children[$j];
+                }               
+            }
+        }
+        return explode(",",implode(",",array_unique($factor_id)));
+    }
+
+    public function getQlist($factors,$paper_id){
+        $this->view->disable();
+        $questions_id = array();
+        for ($i=0; $i <sizeof($factors) ; $i++) {           
+            $factor = Factor::findFirst($factors[$i]);
+            if ($factor->paper_id == $paper_id) {
+                $children = $factor->children;
+                $childrentype = $factor->children_type;
+                $children = explode(",",$children );
+                $childrentype = explode(",", $childrentype);
+                for ($j=0; $j < sizeof($childrentype); $j++) { 
+                    //0代表factor，1代表question
+                    if ($childrentype[$j] == "0") {
+                        $factor1 = Factor::findFirst($children[$j]);
+                        $children1 = $factor1->children;
+                        $children1 = explode(",",$children1);
+                        for ($k=0; $k <sizeof($children1) ; $k++) { 
+                            $children1[$k] = intval($children1[$k]);
+                            $questions_id[] = $children1[$k];                       
+                        }
+                    }
+                    else{   
+                            $children[$j] = intval($children[$j]);
+                            $questions_id[] = $children[$j];
+                    }               
+                }
+            }
+        }
+        return explode(",",implode(",",array_unique($questions_id)));
+    }
+
+    public function getExam($questions){
+        $data = new array();
+        for ($i=0; $i < sizeof($questions); $i++) { 
+
+            $questions = Question::findFirst($questions[$i]);
+            $data[$i]={
+                'index':$i,
+                'title':$questions->topics,
+                'options':$questions->options
+            }
+        }
+        return $data;
+    }
+
+    public function getIds($models)
+    {
+        $id_array = array();
+        foreach ($models as $model) {
+            $id_array[]  = $model->id;
+        }
+        return $id_array;
+    }
+
+    public function getModules($project)
+    {
+        $id_array = array();
+        foreach ($project as $projects) {
+            $id_array[]  = $projects->module_id;
+        }
+        return $id_array;
+    }
+
     public function addAction()
     {
         // $paper = new Paper("select * from paper"); 
