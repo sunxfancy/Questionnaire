68ceb231a0a89387ba2ef35b2a629c677fc8f946
sxf <sun_xfancy@163.com>
2015-08-12 10:24:02 +0800

    Signed-off-by: sxf <sun_xfancy@163.com>

--- app/library/Score.php
+++ app/library/Score.php
@@ -3,7 +3,7 @@
  * @Author: sxf
  * @Date:   2015-08-11 11:08:59
  * @Last Modified by:   sxf
- * @Last Modified time: 2015-08-11 17:38:34
+ * @Last Modified time: 2015-08-11 17:44:47
  */
 
 /**
@@ -31,7 +31,7 @@ class Score
 	// 传入一个answer数组, 计算所有人的得分
 	function calAns($answer)
 	{
-		
+
 	}
 
 
