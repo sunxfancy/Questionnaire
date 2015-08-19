0503dcf4bb9ac1d525acf941ab14fac2f9c98140
liuhenghaha <1185267428@qq.com>
2015-08-19 09:33:59 +0800

    Signed-off-by: liuhenghaha <1185267428@qq.com>

--- app/models/Interview.php
+++ app/models/Interview.php
@@ -51,21 +51,4 @@ class Interview extends \Phalcon\Mvc\Model
     }
     
 
-
-    public function commentSave($arr){
-        $interview = new Interview();
-        $interview->advantage = $arr['advantage'];
-        $interview->disadvantage = $arr['disadvantage'];
-        $interview->remark = $arr['remark'];
-        $interview->examinee_id = $arr['examinee_id'];
-        $interview->manager_id = $arr['manager_id'];
-
-        if($interview->save()){
-            return true;
-        }
-        return false;
-
-    }
-
-
 }
