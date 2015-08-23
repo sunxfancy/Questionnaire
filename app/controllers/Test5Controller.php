bfc4bf37a756a5ed10231ef9440d534e3f12d1ea
liuhenghaha <1185267428@qq.com>
2015-08-23 14:27:21 +0800

    Signed-off-by: liuhenghaha <1185267428@qq.com>

--- app/controllers/Test5Controller.php
+++ app/controllers/Test5Controller.php
@@ -12,8 +12,9 @@ class Test5Controller extends Base
 	public function indexAction(){
 		$std_score = $this->calStd(12);
 		$ans_score = $this->calAns(12);
-		print_r($ans_score);
 		$this->insertScore($std_score,$ans_score,12);
+		// $index_score = $this->calIndex(12);
+		// print_r($index_score);
 	}
 
 	public function insertScore($std_score,$ans_score,$examinee_id){
@@ -204,7 +205,14 @@ class Test5Controller extends Base
 		return $ans_score;
 	}
 
-	public function cal_index($examinee_id){
+	public function calIndex($examinee_id){
+		$factor_ans = FactorAns::findFirst(array(
+    		'examinee_id=?1',
+    		'bind'=>array(1=>$examinee_id)));
+		echo "<pre>";
+		print_r($factor_ans);
+		echo "</pre>";
+		exit();
 		$index_ans = IndexAns::find(array(
 			'examinee_id=?0',
 			'bind'=>array(0=>$examinee_id)));
@@ -212,7 +220,7 @@ class Test5Controller extends Base
 			$factor_score = array();
 			$index = Index::findFirst($index_anses->index_id);
             $children = explode(",",$index->children );          
-            $childrentype = explode(",", $index->childrentype);
+            $childrentype = explode(",", $index->children_type);
             $action = $index->action;
             for ($j=0; $j < sizeof($childrentype); $j++) { 
                 //0代表index，1代表factor
@@ -225,40 +233,28 @@ class Test5Controller extends Base
                     $action = $index1->action;
                     for ($k=0; $k <sizeof($children1) ; $k++) {
                     	$factor_id = CalAge::getFactorId($children1[$k]);
-                    	$factor_ans = FactorAns::findFirst(array(
-                    		'factor_id=?0',
-                    		'bind'=>array(0=>$factor_id)));
-                    	$factor_score[] = $factor_ans->ans_score;
+                    	if ($factor_ans->factor_id == $factor_id) {
+                    		$factor_score[$children1[$k]] = $factor_ans->ans_score;
+                    	}
                     }
-                    $score = $this->doAction($children,$action,$factor_score);
+                    $do_action = preg_replace('/[a-zA-Z][a-zA-Z0-9]*/', '\$factor_score[\'$0\']', $action);
+					$do_action =  "\$index_score = $do_action;";
+					eval($do_action);
+                    $score[$examinee_id][$index1->id] = sprintf('%.2f',$factor_score);
                 }
                 else{   
-                    	$factor_id = CalAge::getFactorId($children[$j]);
-                    	$factor_ans = FactorAns::findFirst(array(
-                    		'factor_id=?0',
-                    		'bind'=>array(0=>$factor_id)));
-                    	$factor_score[] = $factor_ans->ans_score;
+                	$factor_id = CalAge::getFactorId($children[$j]);
+                	if ($factor_ans->factor_id == $factor_id) {
+                		$factor_score[$children1[$k]] = $factor_ans->ans_score;
+                	}
                 }   
-                $score = $this->doAction($children,$action,$factor_score);            
+                $do_action = preg_replace('/[a-zA-Z][a-zA-Z0-9]*/', '\$factor_score[\'$0\']', $action);
+				$do_action =  "\$index_score = $do_action;";
+				eval($do_action);
+                $score[$examinee_id][$index->id] = sprintf('%.2f',$factor_score);            
             }
 		}
-	}
-
-	function doAction($children, $action, $array)
-	{
-		if ($this->action_function[$action] == null) {
-			$this->action_function[$action] = $this->complie_action($children, $action);
-		}
-		return call_user_func_array($this->action_function[$action], $array);
-	}
-
-	function complie_action($child_list, $action)
-	{
-		// 这里需要正则加$符号
-		$child_list = preg_replace('/[a-zA-Z][a-zA-Z0-9]*/', '\$$0', $child_list);
-		$action     = preg_replace('/[a-zA-Z][a-zA-Z0-9]*/', '\$$0', $action);
-		$action = "return $action;";
-		return create_function($child_list, $action);
+		return $score;
 	}
 	
 }
