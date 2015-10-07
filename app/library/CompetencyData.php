e3e4f0f23eb08b9aa36c34b9cf05676965ef8a65
王耀辉 <13240327919@163.com>
2015-10-07 23:39:28 +0800

    Signed-off-by: 王耀è¾? <13240327919@163.com>

--- app/library/CompetencyData.php
+++ app/library/CompetencyData.php
@@ -1,60 +1,100 @@
 <?php
 
 
-class CompetencyData
+class CompetencyData extends \Phalcon\Mvc\Controller
 {
-	public static function getData($project_id,$type){
-		$examinee = Examinee::find(array(
-			'project_id=?0 and team=?1 and state>4',
-			'bind'=>array(0=>$project_id,1=>$type)));
-		$examinee_all = Examinee::find(array(
-			'project_id=?1 and state>4',
-			'bind'=>array(1=>$project_id)));
-		if (count($examinee) == 0) {
-			throw new Exception("还未有".$type."的人参与测试！");
+	/**
+	 * @usage 系统 的胜任力报告 ****除去绿色通道****
+	 * $Project_id int 
+	 * $type 系统
+	 */
+	public function getSystemData($project_id){
+		$team_members = Examinee::find(
+		array("project_id = ?1 AND team = '系统' AND type = 0",
+		'bind'=>array(1=>$project_id)
+		));
+		$members_count = count($team_members) ;
+		if ($members_count == 0 ){
+			throw new Exception('项目中的\'系统\'人数为0，无法进行胜任力报告生成');
 		}
-		foreach ($examinee as $examinees) {
-			$index_score = IndexAns::find(array(
-				'examinee_id=?1',
-				'bind'=>array(1=>$examinees->id)));
-			foreach ($index_score as $iscore) {
-				$score[$iscore->index_id][] = $iscore->score;
+		$members_not_finished = array();
+		foreach($team_members as $value){
+			if ($value->state < 4 ){
+				$tmp = array();
+				$tmp['name'] = $value->name;
+				$tmp['number'] = $value->number;
+				$members_not_finished[] = $tmp;
 			}
 		}
-		foreach ($score as $key => $value) {
-			$sum = array_sum($value);
-			$count = count($value);
-			$avg[$key] = $sum / $count;
+		if (count($members_not_finished) > 0 ) {
+			throw new Exception('系统中部分成员未完成测评过程-名单-'.print_r($members_not_finished,true));
 		}
-		arsort($avg);
-		if (count($avg) > 5) {
-			$advantage = array_slice($avg, 0, 5, true);
+		$result = $this->modelsManager->createBuilder()
+			    ->columns(array(
+				//'Index.name as name',
+				'Index.chs_name as chs_name',
+				'avg(IndexAns.score) as score'
+				))
+			   ->from('Examinee')
+			   ->join('IndexAns', 'IndexAns.examinee_id = Examinee.id AND Examinee.type = 0 AND Examinee.project_id='.$project_id." AND Examinee.team = '系统'")
+			   ->join('Index' , 'Index.id = IndexAns.index_id')
+			   ->groupBy('Index.id')
+			   ->orderBy('avg(IndexAns.score) desc')
+		       ->getQuery()
+		       ->execute();
+		$result = $result->toArray();
+		$rt = array();
+		$result_count = count($result);
+		if ( $result_count > 8 ){
+			$advantage = array_slice($result, 0, 5);
+			$disadvantage = array_slice($result, -3);	
+			$rt['count'] = 8;
+			$rt['advantage']['count'] = 5;
+			$rt['advantage']['value'] = $advantage;
+			$rt['disadvantage']['count'] = 3;
+			$rt['disadvantage']['value'] = $disadvantage;		
 		}else{
-			$advantage = $avg;
-		} 
-		$disadvantage = array_slice($avg, count($avg)-3, 3, true);
-		foreach ($advantage as $key => $value) {
-			$chs_name = Index::findFirst($key)->chs_name;
-			$result['advantage'][$key]['chs_name'] = $chs_name;
-			$result['advantage'][$key]['value'] = $value;
-			$comment = CompetencyComment::findFirst(array(
-				'name=?1',
-				'bind'=>array(1=>$chs_name)))->advantage;
-			$comment = explode("|", $comment);
-	 		$rand_key = array_rand($comment);
-	 		$result['advantage'][$key]['comment'] = $comment[$rand_key];
+			if ($result_count >= 5 ){
+				$rt['count'] = $result_count;
+				$advantage = array_slice($result, 0, 5);
+				$rt['advantage']['count'] = 5;
+				$rt['advantage']['value'] = $advantage;
+				$rt['disadvantage']['count'] = $result_count-5;
+				$disadvantage = array_slice($result, 5, $rt['disadvantage']['count']);
+				$rt['disadvantage']['value'] = $disadvantage;
+			}else{
+				$rt['count'] = $result_count;
+				$advantage = $result;
+				$rt['advantage']['count'] = $result_count;
+				$rt['advantage']['value'] = $advantage;
+				$rt['disadvantage']['count'] =0;
+				$rt['disadvantage']['value'] = null;
+			}
+			
 		}
-		foreach ($disadvantage as $key => $value) {
-			$chs_name = Index::findFirst($key)->chs_name;
-			$result['disadvantage'][$key]['chs_name'] = $chs_name;
-			$result['disadvantage'][$key]['value'] = $value;
+		$sum = 0;
+		foreach($rt['advantage']['value'] as &$value){
+			$value['score'] =  sprintf("%.1f", $value['score']* 12) ;
 			$comment = CompetencyComment::findFirst(array(
-				'name=?1',
-				'bind'=>array(1=>$chs_name)))->disadvantage;
+					'name=?1',
+					'bind'=>array(1=>$value['chs_name'])))->advantage;
 			$comment = explode("|", $comment);
-	 		$rand_key = array_rand($comment);
-	 		$result['disadvantage'][$key]['comment'] = $comment[$rand_key];
+			$rand_key = array_rand($comment);
+			$value['comment'] = $comment[$rand_key];
+			$sum += $value['score'];
 		}
-		return $result;
+		foreach($rt['disadvantage']['value'] as &$value){
+			$value['score'] =  sprintf("%.1f", $value['score']* 12) ;
+			$comment = CompetencyComment::findFirst(array(
+					'name=?1',
+					'bind'=>array(1=>$value['chs_name'])))->disadvantage;
+			$comment = explode("|", $comment);
+			$rand_key = array_rand($comment);
+			$value['comment'] = $comment[$rand_key];
+			$sum += $value['score'];
+		}
+		$number = $rt['count'];
+		$rt['value'] =sprintf('%.1f',($sum / $number) ); 
+		return $rt;
 	}
 }
