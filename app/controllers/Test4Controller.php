284aa47e2cb371a20348dd3cb2b7a6b874e2babc
王耀辉 <13240327919@163.com>
2015-10-23 18:38:14 +0800

    Signed-off-by: 王耀è¾? <13240327919@163.com>

--- app/controllers/Test4Controller.php
+++ app/controllers/Test4Controller.php
@@ -8,22 +8,34 @@ class Test4Controller extends \Phalcon\Mvc\Controller{
 	
 	public function indexAction(){		
 			
-		$examinee_id =2998;
-		$examinee = Examinee::findFirst($examinee_id);
-		$excel = new CheckoutExcel();
-		$report = $excel->excelExport($examinee);
+		$project_id = 1521;
+		$da  = new ProjectDataExport();
+		$re = $da->excelExport($project_id);
+		
 
 	}
 	public function testAction(){
 // 		$check_data = new CheckoutData();
 // 		$examinee = Examinee::findFirst(2660);
 // 		$rt = $check_data->getEightAddFive($examinee);
-		$da = new CheckoutData();
-		$rt = $da->getIndexdesc(2998);
+		$da = new ProjectData();
+		$rt = $da->getindividualComprehensive(2998);
 		echo '<pre>';
 		print_r($rt);
 // 		$check_data->getChildrenOfIndexDesc('zb_gzzf', 'X4,zb_rjgxtjsp,chg,Y3,Q3,spmabc,aff',2660);
 	}
-	
+	public function test2Action(){
+		$da = new ProjectData();
+		$rt = $da->getindividualComprehensive(2996);
+		echo '<pre>';
+		print_r($rt);
+	}
+	public function test3Action(){
+		$da = new ProjectData();
+		$info = Examinee::findFirst(2997);
+		$rt = $da->getEightAddFive($info);
+		echo '<pre>';
+		print_r($rt);
+	}
 
 }
